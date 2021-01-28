bf80e62ca3e9366bddb966af1ffe10f8fc90dce9
Stanley Muyuga <stanelymyuga.12@gmail.com>
2021-01-28 16:58:07 +0300

    .....

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -199,4 +199,34 @@ class AuthController extends Controller
         $user_plans = UserPlans::create($validatedData);
         return response(['user_plans' => $user_plans, 'message' => 'Created Successfully'], 201);
     }
+
+    protected function send_message(String $to, String $from, String $message)
+    {
+        $username = "Shopilyv";
+        $password = "WorthBill2030$";
+
+        $auth = base64_encode($username . ":" . $password);
+        $headers = ['Authorization' => 'Basic ' . $auth, 'Content-Type' => ' application/json', 'Accept' => 'application/json',];
+        $api_url = 'https://api.infobip.com/sms/2/text/single';
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $api_url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "{ \"from\":\"$from\", \"to\":\"$to\", \"text\":\"$message.\" }",
+            CURLOPT_HTTPHEADER => array(
+                "accept: application/json",
+                "authorization: Basic " . json_encode($auth),
+                "content-type: application/json"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+    }
+
+    
 }
